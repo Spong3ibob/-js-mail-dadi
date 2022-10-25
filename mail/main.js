8dcd428544bc44b0aef23f45b6b2c0c1e81cc696
Spong3ibob <leonardomaran17@gmail.com>
2022-10-25 16:19:46 +0200

    aggiunto includes

--- mail/main.js
+++ mail/main.js
@@ -1,8 +1,7 @@
 const email = prompt ("inserisi la tua e mail");
-const emailVerified = ["marcorossi@gmail.com"]
-console.log(email);
-if(email == emailVerified){
-   console.log("Passed")
+const emailVerified = ["marcorossi@gmail.com", "andreabinachi@gmail.com", "paolocannone@gmail.com"];
+if(email.includes("marcorossi@gmail.com", "andreabinachi@gmail.com", "paolocannone@gmail.com")){
+    console.log("Passed")
 }else{
-   console.log("Negated")
+    console.log("Negated")
 }
