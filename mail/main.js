ee065fc57555ac515ff08f27a811344cd9cd413e
Spong3ibob <leonardomaran17@gmail.com>
2022-10-25 16:32:03 +0200

    completato esercizzio e- mail

--- mail/main.js
+++ mail/main.js
@@ -1,6 +1,10 @@
 const email = prompt ("inserisi la tua e mail");
 const emailVerified = ["marcorossi@gmail.com", "andreabinachi@gmail.com", "paolocannone@gmail.com"];
-if(email.includes("marcorossi@gmail.com", "andreabinachi@gmail.com", "paolocannone@gmail.com")){
+if(email.includes("marcorossi@gmail.com")){
+    console.log("Passed Admin")
+}else if(email.includes("andreabinachi@gmail.com")){
+    console.log("Passed")
+}else if(email.includes("paolocannone@gmail.com")){
     console.log("Passed")
 }else{
     console.log("Negated")
