41fa87116a30f6e13ca70073d6cb33bfda58e602
Spong3ibob <leonardomaran17@gmail.com>
2022-10-25 15:42:19 +0200

    esercizzio email

---
+++ mail/main.js
@@ -0,0 +1,8 @@
+const email = prompt ("inserisi la tua e mail");
+const emailVerified = ["marcorossi@gmail.com"]
+console.log(email);
+if(email == emailVerified){
+   console.log("Passed")
+}else{
+   console.log("Negated")
+}
